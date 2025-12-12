ff8713c81e931d85efc32b4f6d60022da5268566
Show Wai Yan <showwaiyan555@gmail.com>
2025-12-12 23:11:37 +0800

    Handle video stop function when user close modal window

--- Services/CameraServices.ts
+++ Services/CameraServices.ts
@@ -1,7 +1,7 @@
 export class Camera {
 	parentEl: HTMLElement;
 	stream: MediaStream;
-	videoElement: HTMLElement;
+	videoElement: HTMLVideoElement;
 
 	constructor(parentEl: HTMLElement) {
 		this.parentEl = parentEl;
@@ -13,10 +13,23 @@ export class Camera {
 			video: true,
 			audio: false,
 		});
+		this.videoElement.srcObject = this.stream;
+		try {
+			await this.videoElement.play();
+		} catch (error) {
+			// Ignore play interruption errors (DOMException)
+			if (error.name !== 'AbortError') {
+				throw error;
+			}
+		}
 	}
 
 	detachCamera() {
 		if (!this.stream) return;
+		// Pause video and clear srcObject
+		this.videoElement.pause();
+		this.videoElement.srcObject = null;
+		// Stop all media tracks
 		const tracks = this.stream.getTracks();
 		tracks.forEach((track) => {
 			track.stop();
